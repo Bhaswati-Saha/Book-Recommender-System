f3c34b8c71dd95a416a987560aa59215d4565c68
Bhaswati-Saha <bhaswati.sh12@gmail.com>
2021-04-22 07:42:27 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import recommendation
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+@app.route('/recommend', methods=['post'])
+def predict():
+    book_name=request.form.get('title')
+    if book_name not in recommendation.book_pivot.index:
+        response = ['Book not found']
+    else:
+        response = recommendation.recommend_book(book_name)
+
+    return render_template("index.html",response=response)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
